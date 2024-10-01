6ad95c81df0f60853838464afd62804be2ee698b
Ishita <ishitajain385@gmail.com>
2024-10-01 22:08:55 +0530

    cart and bill generation

--- src/store.jsx
+++ src/store.jsx
@@ -1,57 +1,4 @@
-// import { Provider } from "react-redux";
-// import { createStore } from "redux";
-// const initialState= {
-//     listofItems:["aquarium","filter","water filter","air filter","husky","dog","cat","shepherd","shitzu","dog collar","collar","collars","dog collars","dogs","cat collars","cats","bowls","dog bowls","cat bowls",
-//       "leash","bowl","tub","bath tub"
-//     ],
-//     wordsearched:[],
-// itemCount:0,
-//     isHome:false,
-//     isBack:false,
 
-// }
-// const reducerfn=(state=initialState,action)=>{
-// switch(action.type){
-
-//           case "SEARCH_ITEMS":
-//             const searched= action.payload.trim()
-//             if(searched === "" ){
-//           return {
-//                 ...state,
-//                 wordsearched: [],
-//               };
-              
-             
-//             }
-
-
-//   const filteredItems = state.listofItems.filter((item) =>
-//     item.toLowerCase().includes(searched.toLowerCase())
-//   );
-//   return {
-//     ...state,
-//     wordsearched: filteredItems,
-//   };
-
-//   case "NAVIGATOR":
-//     return{
-//       ...state,
-//       isHome:action.payload.isHome,
-//       isBack:action.payload.isBack,
-//     }
-
-           
-            
-           
-      
-//           default: return state
-//         }
-//       };
-//  const store=createStore(reducerfn)
-
-// export const StoreProvider=({children})=>{
-//    return <Provider store={store}>{children}</Provider>
-// }
 
 
 
@@ -65,10 +12,13 @@ const initialState= {
 itemCount:0,
     isHome:false,
     isBack:false,
+    productadded:[],
+    productprices:[]
 
 }
 const reducerfn=(state=initialState,action)=>{
   console.log(action)
+  
 switch(action.type){
           case "SEARCH_ITEMS":
             const searched= action.payload.trim()
@@ -100,13 +50,16 @@ switch(action.type){
            
         case "ITEM_ADDED"   : return{
           ...state,
-          itemCount:state.itemCount+1
+          itemCount:state.itemCount+1,
+          productadded:  [...state.productadded, action.payload.item],
+          productprices:  [...state.productprices, action.payload.pay],
         } 
-           
+
       
           default: return state
         }
       };
+
  const store=createStore(reducerfn)
 
 export const StoreProvider=({children})=>{
